3c061d1a56a2e1e70322999a1454c1d488681360
Hardik Jaishi <hardikjaishi1120@gmail.com>
2022-07-25 17:43:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       Deployment is happening here!
+       My demo app in building!
       </header>
     </div>
   );
